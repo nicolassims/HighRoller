88a9303f2145e22b988af88c05e006ebda5021e2
nicolassims <sableye.nico@gmail.com>
2017-01-09 14:34:00 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+/*  AUTHOR: Nicolas Sims
+ *  VERSION: 2.4.7
+ *  CREATED: 1/9/2017
+ *  PURPOSE: Create a dice roller
+ */
+
+'use strict';
+
+class main {
+    constructor() {
+
+    }
+}
+
+window.onload = function() {
+    new main();
+};
