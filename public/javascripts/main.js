d789979dd3e602e7de471cbb093dbf71674822ab
nicolassims <sableye.nico@gmail.com>
2017-01-16 15:10:21 -0500

    Got javascript working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,12 +9,31 @@
 class main {
     constructor() {
         setValues.readyApp();
+        performFunction.rollDice();
     }
 }
 
 class setValues {
     static readyApp() {
         document.getElementById("defaultTypeDice").checked = true;
+    }
+}
+
+class performFunction {
+    static rollDice() {
+        document.getElementById("button").addEventListener("click", () => {
+            const ANSWERS = document.forms['theForm'].elements['typeDice'];
+            for (let i = 0; i < ANSWERS.length; i++) {
+                if (ANSWERS[i].checked == true) {
+                    let typeDice = ANSWERS[i].value;
+                    console.log("typeDice=" + typeDice);
+                    let numberDice = Number(document.getElementById("numberDice").value);
+                    console.log("numberDice=" + numberDice);
+                    let result = typeDice * numberDice;
+                    console.log("result=" + result);
+                }
+            }
+        }, false);
     }
 }
 
