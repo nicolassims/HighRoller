ad9007d9b00cc2dde241e61d1a5a540316716ada
Nicolas Sims <sableye.nico@gmail.com>
2017-01-17 18:30:51 -0500

    Essentially done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,21 +16,40 @@ class main {
 class setValues {
     static readyApp() {
         document.getElementById("defaultTypeDice").checked = true;
+        document.getElementById("defaultTypeResults").checked = true;
+        document.getElementById("numberDice").value = 1;
     }
 }
 
 class performFunction {
     static rollDice() {
+        const SECONDANSWERS = document.forms['theSecondForm'].elements['typeResults'];
+        const ANSWERS = document.forms['theForm'].elements['typeDice'];
+        let results = [];
+        let result = 0;
         document.getElementById("button").addEventListener("click", () => {
-            const ANSWERS = document.forms['theForm'].elements['typeDice'];
+            document.getElementById("diceResults").value = '';
             for (let i = 0; i < ANSWERS.length; i++) {
                 if (ANSWERS[i].checked == true) {
-                    let typeDice = ANSWERS[i].value;
-                    console.log("typeDice=" + typeDice);
-                    let numberDice = Number(document.getElementById("numberDice").value);
-                    console.log("numberDice=" + numberDice);
-                    let result = typeDice * numberDice;
-                    console.log("result=" + result);
+                    for (let j = 0; j < Number(document.getElementById("numberDice").value); j++) {
+                        results[j] = Math.floor((Math.random() * ANSWERS[i].value) + 1);
+                    }
+                    if (SECONDANSWERS[0].checked == true) {
+                        for (let i = 0; i < results.length; i++) {
+                            result += results[i];
+                            console.log(result);
+                        }
+                        document.getElementById("diceResults").value = String(result);
+                        result = 0;
+                    } else {
+                        for (let i = 0; i < results.length; i++) {
+                            if (i < results.length - 1) {
+                                document.getElementById("diceResults").value += results[i] + ', '
+                            } else {
+                                document.getElementById("diceResults").value += results[i]
+                            }
+                        }
+                    }
                 }
             }
         }, false);
